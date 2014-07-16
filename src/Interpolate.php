05233b6d1cbdaec90ff0a2c8a929f1471c384624
yuanqing <hello@yuanqing.sg>
2014-07-16 10:33:37 +0800

    Going back to single quoted strings

--- src/Interpolate.php
+++ src/Interpolate.php
@@ -25,11 +25,11 @@ class Interpolate
   public function render($tmpl, array $data)
   {
     if (!$this->isString($tmpl)) {
-      throw new \InvalidArgumentException("Template could not be converted to string");
+      throw new \InvalidArgumentException('Template could not be converted to string');
     }
     $tmpl = (string) $tmpl;
     $this->data = $data;
-    return preg_replace_callback("/{{(.+?)}}/", array($this, "callback"), $tmpl);
+    return preg_replace_callback('/{{(.+?)}}/', array($this, 'callback'), $tmpl);
   }
 
   /**
@@ -41,14 +41,14 @@ class Interpolate
    */
   private function callback($matches)
   {
-    $keys = explode(".", $matches[1]);
+    $keys = explode('.', $matches[1]);
     $val = $this->followPath($this->data, $keys);
     if (is_callable($val)) {
       $val = call_user_func($val, $this->data);
     }
     if (!$this->isString($val)) {
-      throw new \UnexpectedValueException(sprintf("Value corresponding to the key '%s' could not
-        be converted to string", trim($matches[1])));
+      throw new \UnexpectedValueException(sprintf('Value corresponding to the key \'%s\' could not
+        be converted to string', trim($matches[1])));
     }
     return (string) $val;
   }
@@ -57,9 +57,9 @@ class Interpolate
    * Get a value from $data by following the path specified by the series of keys in $keys
    *
    * @example
-   * $data = array("foo" => array("bar" => "baz");
-   * $keys = array("foo", "bar");
-   * var_dump($this->followPath($data, $keys)); #=> "baz"
+   * $data = array('foo' => array('bar' => 'baz');
+   * $keys = array('foo', 'bar');
+   * var_dump($this->followPath($data, $keys)); #=> 'baz'
    * @param array $data Array to get the value from
    * @param array $keys Series of keys to follow
    * @return mixed|null The value from $data corresponding to the path, or null if the path
@@ -85,7 +85,7 @@ class Interpolate
    */
   private function isString($obj)
   {
-    return is_scalar($obj) || is_null($obj) || (is_object($obj) && method_exists($obj, "__toString"));
+    return is_scalar($obj) || is_null($obj) || (is_object($obj) && method_exists($obj, '__toString'));
   }
 
 }
--- test/InterpolateTest.php
+++ test/InterpolateTest.php
@@ -7,7 +7,7 @@
  * @link https://github.com/yuanqing/render
  */
 
-require_once dirname(__DIR__) . "/src/Interpolate.php";
+require_once dirname(__DIR__) . '/src/Interpolate.php';
 
 use yuanqing\Interpolate\Interpolate;
 
@@ -21,7 +21,7 @@ class InterpolateTest extends PHPUnit_Framework_TestCase
     $i = new Interpolate;
     $tmpl = array();
     $data = array(
-      "foo" => "bar"
+      'foo' => 'bar'
     );
     $i->render($tmpl, $data);
   }
@@ -32,86 +32,86 @@ class InterpolateTest extends PHPUnit_Framework_TestCase
   public function testInvalidDataArgument()
   {
     $i = new Interpolate;
-    $tmpl = "{{ foo }}";
+    $tmpl = '{{ foo }}';
     $data = null;
-    $i->render("{{ foo }}", $data);
+    $i->render('{{ foo }}', $data);
   }
 
   public function testEmptyTemplate()
   {
     $i = new Interpolate;
     $data = array();
-    $this->assertEquals($i->render(null, $data), "");
-    $this->assertEquals($i->render("", $data), "");
+    $this->assertEquals($i->render(null, $data), '');
+    $this->assertEquals($i->render('', $data), '');
   }
 
   public function testEmptyData()
   {
     $i = new Interpolate;
-    $tmpl = "{{ foo }}";
-    $this->assertEquals($i->render($tmpl, array()), "");
-    $this->assertEquals($i->render($tmpl, array("foo" => null)), "");
+    $tmpl = '{{ foo }}';
+    $this->assertEquals($i->render($tmpl, array()), '');
+    $this->assertEquals($i->render($tmpl, array('foo' => null)), '');
   }
 
   public function testStringTagName()
   {
     $i = new Interpolate;
     $data = array(
-      "foo" => "bar"
+      'foo' => 'bar'
     );
-    $this->assertEquals($i->render("{{ foo }}", $data), "bar");
-    $this->assertEquals($i->render("{{foo}}", $data), "bar");
+    $this->assertEquals($i->render('{{ foo }}', $data), 'bar');
+    $this->assertEquals($i->render('{{foo}}', $data), 'bar');
   }
 
   public function testWhitespaceWithinTagName()
   {
     $i = new Interpolate;
     $data = array(
-      "foo bar" => "baz"
+      'foo bar' => 'baz'
     );
-    $this->assertEquals($i->render("{{ foo bar }}", $data), "baz");
-    $this->assertEquals($i->render("{{foo bar}}", $data), "baz");
+    $this->assertEquals($i->render('{{ foo bar }}', $data), 'baz');
+    $this->assertEquals($i->render('{{foo bar}}', $data), 'baz');
   }
 
   public function testNumericTagName()
   {
     $i = new Interpolate;
-    $tmpl = "{{ 0 }}";
+    $tmpl = '{{ 0 }}';
     $data = array(
-      0 => "foo"
+      0 => 'foo'
     );
-    $this->assertEquals($i->render($tmpl, $data), "foo");
+    $this->assertEquals($i->render($tmpl, $data), 'foo');
   }
 
   public function testGlobalReplace()
   {
     $i = new Interpolate;
-    $tmpl = "{{ foo }} {{ foo }}";
+    $tmpl = '{{ foo }} {{ foo }}';
     $data = array(
-      "foo" => "bar",
+      'foo' => 'bar',
     );
-    $this->assertEquals($i->render($tmpl, $data), "bar bar");
+    $this->assertEquals($i->render($tmpl, $data), 'bar bar');
   }
 
   public function testNumericValue()
   {
     $i = new Interpolate;
-    $tmpl = "{{ foo }}{{ bar }}";
+    $tmpl = '{{ foo }}{{ bar }}';
     $data = array(
-      "foo" => 3.14,
-      "bar" => 159
+      'foo' => 3.14,
+      'bar' => 159
     );
-    $this->assertEquals($i->render($tmpl, $data), "3.14159");
+    $this->assertEquals($i->render($tmpl, $data), '3.14159');
   }
 
   public function testObjectValueWithToString()
   {
     $i = new Interpolate;
-    $tmpl = "{{ foo }}";
+    $tmpl = '{{ foo }}';
     $data = array(
-      "foo" => new SplFileInfo("bar") # SplFileInfo implements __toString()
+      'foo' => new SplFileInfo('bar') # SplFileInfo implements __toString()
     );
-    $this->assertEquals($i->render($tmpl, $data), "bar");
+    $this->assertEquals($i->render($tmpl, $data), 'bar');
   }
 
   /**
@@ -120,9 +120,9 @@ class InterpolateTest extends PHPUnit_Framework_TestCase
   public function testObjectValueWithoutToString()
   {
     $i = new Interpolate;
-    $tmpl = "{{ foo }}";
+    $tmpl = '{{ foo }}';
     $data = array(
-      "foo" => new StdClass # StdClass does not implement __toString()
+      'foo' => new StdClass # StdClass does not implement __toString()
     );
     $i->render($tmpl, $data);
   }
@@ -133,10 +133,10 @@ class InterpolateTest extends PHPUnit_Framework_TestCase
   public function testArrayValue()
   {
     $i = new Interpolate;
-    $tmpl = "{{ foo }}";
+    $tmpl = '{{ foo }}';
     $data = array(
-      "foo" => array(
-        "bar" => "baz"
+      'foo' => array(
+        'bar' => 'baz'
       )
     );
     $i->render($tmpl, $data);
@@ -146,29 +146,29 @@ class InterpolateTest extends PHPUnit_Framework_TestCase
   {
     $i = new Interpolate;
     $data = array(
-      "foo" => "qux",
-      "bar" => "quux",
-      "baz" => function($data) {
-        return sprintf("%s %s", $data["foo"], $data["bar"]);
+      'foo' => 'qux',
+      'bar' => 'quux',
+      'baz' => function($data) {
+        return sprintf('%s %s', $data['foo'], $data['bar']);
       }
     );
-    $tmpl = "{{ baz }}";
-    $this->assertEquals($i->render($tmpl, $data), "qux quux");
+    $tmpl = '{{ baz }}';
+    $this->assertEquals($i->render($tmpl, $data), 'qux quux');
   }
 
   public function testNestedKeys()
   {
     $i = new Interpolate;
     $data = array(
-      "foo" => array(
-        "bar baz" => array(
-          "qux" => "quux"
+      'foo' => array(
+        'bar baz' => array(
+          'qux' => 'quux'
         )
       )
     );
-    $this->assertEquals($i->render("{{ foo.bar baz.qux }}", $data), "quux");
-    $this->assertEquals($i->render("{{foo . bar baz . qux}}", $data), "quux");
-    $this->assertEquals($i->render("{{ foo. bar baz .qux }}", $data), "quux");
+    $this->assertEquals($i->render('{{ foo.bar baz.qux }}', $data), 'quux');
+    $this->assertEquals($i->render('{{foo . bar baz . qux}}', $data), 'quux');
+    $this->assertEquals($i->render('{{ foo. bar baz .qux }}', $data), 'quux');
   }
 
 }
